77c8a11abd8d8090f97f656e59a7c96efb399c93
Iliyan Ivanov <109862097+oldbasic@users.noreply.github.com>
2023-05-09 22:42:51 +0300

    footer update

--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -5,37 +5,77 @@ import InstagramIcon from '@mui/icons-material/Instagram';
 import TwitterIcon from '@mui/icons-material/Twitter';
 import GoogleIcon from '@mui/icons-material/Google';
 import GitHubIcon from '@mui/icons-material/GitHub';
-
+import ArrowDropUpIcon from '@mui/icons-material/ArrowDropUp';
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
+	const handleUpButton = () => {
+		window.scrollTo({
+			top: 0,
+			behavior: 'smooth',
+		});
+	};
 	return (
 		<div className='footer'>
 			<div className='social-links'>
 				<p>Get connected with us on social networks:</p>
 				<div className='social'>
-					<span className='fb'>
+					<span>
 						<FacebookIcon fontSize='large' />
 					</span>
-					<span className='ig'>
+					<span>
 						<InstagramIcon fontSize='large' />
 					</span>
-					<span className='tw'>
+					<span>
 						<TwitterIcon fontSize='large' />
 					</span>
-					<span className='go'>
+					<span>
 						<GoogleIcon fontSize='large' />
 					</span>
-					<span className='gh'>
+					<span>
 						<GitHubIcon fontSize='large' />
 					</span>
 				</div>
 			</div>
-			<div className='links-wrapper'>
-				<div className='company'></div>
-				<div className='footer-nav'></div>
-				<div className='contacts'></div>
+			<div className='links-gym'>
+				<div className='company'>
+					<img
+						className='logo-footer'
+						src='https://svgshare.com/i/sFF.svg'
+						alt='logo'
+					/>
+				</div>
+				<div className='footer-nav'>
+					<h3>Navigation</h3>
+					<div>
+						<Link to='/'>
+							<span>Home</span>
+						</Link>
+						<Link to='/search'>
+							<span>Spot</span>
+						</Link>
+						<Link to='/about'>
+							<span>About</span>
+						</Link>
+						<Link to='/profile'>
+							<span>Profile</span>
+						</Link>
+					</div>
+				</div>
+				<div className='contacts'>
+					<h3>Contacts:</h3>
+					<span>Враца</span>
+					<span>gymSpotter@yahoo.com</span>
+					<span>+ 359 98 836 0425</span>
+				</div>
 			</div>
-			<div className='copy-right'></div>
+			<div className='copy-right'>
+				<h3>© 2023 Copyright: GymSpotter.com</h3>
+				<button onClick={handleUpButton}>
+					<ArrowDropUpIcon />
+					UP
+				</button>
+			</div>
 		</div>
 	);
 };
